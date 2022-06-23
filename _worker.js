3b073d3a12f4a799dbfbf3f5985d36c6f52c8dee
decadeicey <96861853+decadeicey@users.noreply.github.com>
2022-06-23 10:57:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="abaaba-decadeicey.koyeb.app";
+        url.hostname="wp.jiojioice.repl.co";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
